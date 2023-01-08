4f6dee9fccabb10b7a286878e1c6178f909525ff
Temur Vegas <timanaimoff@gmail.com>
2023-01-08 16:42:53 +0500

    @GetMapping,@RequestMapping and other yummys

--- src/main/java/edu/javacourse/tomcat/controllers/FirstTheController.java
+++ src/main/java/edu/javacourse/tomcat/controllers/FirstTheController.java
@@ -3,16 +3,22 @@ package edu.javacourse.tomcat.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller("firstTeController")
 @RequestMapping("/first")
 public class FirstTheController {
-    @GetMapping("/hello")
-    public String helloPage(){
+    @GetMapping("/hello")   //Catching a param-request with annotations
+    public String helloPage(@RequestParam(value="name",required = false)String name,
+                            @RequestParam(value="surname",required=false)String surName){
+        System.out.println("Hello, "+name+":"+surName);
         return "first/hello";
     }
-    @GetMapping("/goodbye")
-    public String goodByePage(){
+    @GetMapping("/goodbye")  //Cathcing a param-request with HttpServletRequest
+    public String goodByePage(HttpServletRequest params){
+        System.out.println(params.getParameter("name")+":"+params.getParameter("surname"));
         return "first/goodbye";
     }
 }
